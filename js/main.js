57408d7767df3c6744c636bf2eb25ade48603fbb
Sam Singels <s.singels@sintlucasedu.nl>
2023-01-04 13:46:48 +0100

    Footer fix

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ $(".title3").on('click',function(){
 $(".title4").on('click',function(){
   $(this).parent().parent().find('.body4').slideToggle();
 });
+$(".title5").on('click',function(){
+  $(this).parent().parent().find('.body5').slideToggle();
+});
+$(".title6").on('click',function(){
+  $(this).parent().parent().find('.body6').slideToggle();
+});
+$(".title7").on('click',function(){
+  $(this).parent().parent().find('.body7').slideToggle();
+});
+$(".title8").on('click',function(){
+  $(this).parent().parent().find('.body8').slideToggle();
+});
+$(".title9").on('click',function(){
+  $(this).parent().parent().find('.body9').slideToggle();
+});
+$(".title10").on('click',function(){
+  $(this).parent().parent().find('.body10').slideToggle();
+});
+
+
